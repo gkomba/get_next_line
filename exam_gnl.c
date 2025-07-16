d71085cc51264ec48ec315f8d5371e6966206a04
Gildo Komba <161140108+gkomba@users.noreply.github.com>
2025-07-16 18:46:05 +0100

    Create exam_gnl.c

---
+++ exam_gnl.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkomba <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 14:59:09 by gkomba            #+#    #+#             */
+/*   Updated: 2024/08/30 14:59:12 by gkomba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	i = -1;
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		line[70000];
+	static int	buffer_readed;
+	static int 	buffer_pos;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buffer_pos >= buffer_readed)
+		{
+			buffer_readed = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_readed <= 0)
+				break ;
+		}
+		line[i++] = buffer[buffer_pos++];
+		if (line[i - 1] == '\n')
+			break ;
+	}
+	line[i] = '\0';
+	if (i == 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
